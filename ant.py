6a97cf5fa0c8fec3f8aa6f588c86a244e352282b
Victor <victorbultez2@gmail.com>
2023-12-03 18:48:02 +0100

    starting to add  implementations for the ant class

--- ant.py
+++ ant.py
@@ -1,16 +1,32 @@
 import numpy as np
 from graph import Graph
 
-
-
 class Ant:
-    def __init__(self,graph=Graph):
-        # the agent's "position" is a sequence of nodes
-        self.graph = graph  
-        self.vehicule_load=0
+    def __init__(self, graph=Graph,start_node=0):
+        self.graph = graph
+        self.vehicle_load = 0
+        self.current_node = start_node
+        self.visited_nodes = [start_node] 
+        self.route = [start_node]
 
-    def route_construction():
+    def route_construction(self):
+        # Implement the route construction logic here
+        pass
+
+    def move_to_next_node(self,next_node):
+        self.route.append(next_node)
+        self.visited_nodes.append(next_node)
         
-        #Base function gave by the teacher
+        distance = self.graph.distanceMatrix[self.current_node][next_node]
         
+        self.vehicle_load += self.graph.nodes[next_node].demand
+        
+        #How do we know if we are at the depot/end of path?
+        
+                
+        # Implement the move to next node logic here
+        self.current_node = next_node
+
+    def insert_on_path(self, node):
+        # Implement the insertion logic here
         pass
