e59670f3e14c31e4f574ee1ff8e7f8f8cad51450
Victor <victorbultez2@gmail.com>
2023-12-03 16:21:48 +0100

    Just a skeleton of the ant class if needed

---
+++ ant.py
@@ -0,0 +1,16 @@
+import numpy as np
+from graph import Graph
+
+
+
+class Ant:
+    def __init__(self,graph=Graph):
+        # the agent's "position" is a sequence of nodes
+        self.graph = graph  
+        self.vehicule_load=0
+
+    def route_construction():
+        
+        #Base function gave by the teacher
+        
+        pass
